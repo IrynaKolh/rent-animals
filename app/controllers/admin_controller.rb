c19b6d060c3a4927f6c41a193fe9b62efbb8dfaa
Iryna Kolhanova <iryna.kolhanova@gmail.com>
2024-09-26 15:57:26 -0400

    add admin devise

---
+++ app/controllers/admin_controller.rb
@@ -0,0 +1,12 @@
+class AdminController < ApplicationController
+    before_action :check_admin_priv
+    def show
+    end
+    private
+    def check_admin_priv
+      unless current_admin
+        flash[:alert] = "You do not have permission to access this page."
+        redirect_to root_path
+      end
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :admins
   resources :pets
   devise_for :users
+
+  resource :admin, only: [:show], controller: :admin
+
   root 'pages#index'
   get 'pages/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
