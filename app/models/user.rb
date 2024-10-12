dca64299c46775b544ee04dd5ced3cc3fd21893b
Iryna Kolhanova <iryna.kolhanova@gmail.com>
2024-10-12 10:27:08 -0400

    add search

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true, email: true
-  validates :password, password_strength: true, if: -> { password.present? && password_changed? }
+  validates :password, password_strength: true, if: :password_required?
   validates :seller, inclusion: { in: [true, false] }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
@@ -8,4 +8,12 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :pets
+
+  private
+
+  def password_required?
+    # A password is required if the user is new or if the password has been changed
+    new_record? || password.present?
+  end
+  
 end
